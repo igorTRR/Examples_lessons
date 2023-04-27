b0432bcd850a2d89c5b1d40f63b803cb326c0dc1
Igor <tararuev65@gmail.com>
2023-04-27 10:36:40 +0600

    Ex003_Деление

---
+++ Ex_les003_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// деление чисел : init - челочисленное. double - с остатком
+int numberA = 15; //double numberA = 12;
+int numberB = 5;  //double numberA = 5;
+//int result = numberA / numberB;
+Console.WriteLine(numberA / numberB); //(result)
