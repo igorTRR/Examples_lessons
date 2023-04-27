44ef2ef30a9dde15f013ac59e871fc1a05890627
Igor <tararuev65@gmail.com>
2023-04-27 19:51:56 +0600

    MaxМассива

---
+++ Ex_les009ArrayMax/Program.cs
@@ -0,0 +1,21 @@
+﻿// Мах число массива
+
+Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array  = {11,21,131,341,15,61,17,98,19};
+
+int result = Max(
+             Max(array[0], array[1], array[2]),
+             Max(array[3], array[4], array[5]),
+             Max(array[6], array[7], array[8]));
+
+
+Console.WriteLine(result);
