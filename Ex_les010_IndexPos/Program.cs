7842a00b05e349b624c684dadec428f37ed0c735
Igor <tararuev65@gmail.com>
2023-04-27 19:59:46 +0600

    Индекс заданного числа

---
+++ Ex_les010_IndexPos/Program.cs
@@ -0,0 +1,18 @@
+﻿// Нахождение индекса заданного числа
+int[] array = {1,8,23,41,5,18,27,18};
+
+int n = array.Length;
+
+int find = 8;
+int index = 0;
+
+while ( index < n )
+{
+    if(array[index] == find) 
+    {
+       Console.WriteLine(index); 
+       break;  // команда - break;- останавлиает цикл при первом нахождении (индекса) заданного числа (если схожих чисел в [] более1 )
+    }             
+    index ++;  // index= index+1
+}
+
