64d5ad31525ee64db358e29c170ed26a9fbd086f
Igor <tararuev65@gmail.com>
2023-04-27 10:52:00 +0600

    Ex005_МАША!

---
+++ Ex_les005_Masha!/Program.cs
@@ -0,0 +1,13 @@
+﻿// Приветствие - знакомое имя -!..незнакомое -привет...
+Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //ToLower() - убирает все варианты маша (Маша=мАша =МАша итд)
+{
+    Console.WriteLine("Yes! это МАША!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username );
+}
