1ff6782e621b75abf51f5326ac17a2005c77703d
Igor <tararuev65@gmail.com>
2023-04-27 10:43:56 +0600

    Ex004Сумма произв чисел массива

---
+++ Ex_les004_Random/Program.cs
@@ -0,0 +1,7 @@
+﻿//  Создаем массив рандомных чисел и сумму 2х произвольных чисел
+int numberA = new Random().Next(1,10); // 1,2,3,4....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
