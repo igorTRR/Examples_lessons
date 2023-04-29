c4987e3e02b2f1e596501ec6977d266f10e17aee
Igor <tararuev65@gmail.com>
2023-04-29 23:09:34 +0600

    Квадрат числа

---
+++ Ex_Sem001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, 
+//является ли первое число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.Clear();
+
+Console.WriteLine("Введите число а: " );
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b: " );
+int b = int.Parse(Console.ReadLine()!);
+
+if(b*b == a)
+{
+    Console.WriteLine($"Yes ");
+}
+else 
+{
+    Console.WriteLine($"No ");
+}
