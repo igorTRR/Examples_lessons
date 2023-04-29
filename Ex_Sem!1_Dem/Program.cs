a8b31776ae8366023fe0698bba2efd6945b2fcb2
Igor <tararuev65@gmail.com>
2023-04-29 09:51:05 +0600

    Нахождение sqr числа

--- Ex_Sem!1_Dem/Program.cs
+++ Ex_Sem!1_Dem/Program.cs
@@ -7,4 +7,6 @@ Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
 
 int sqr = number * number;
+double sqr2 = Math.Pow(number, 2);
 Console.WriteLine($"квадрат {number} = {sqr}");
+ Console.WriteLine($"квадрат {number} = {sqr2}");
