d8e80ce239de491b9cbadb0d1d0e10dadf05f107
Igor <tararuev65@gmail.com>
2023-04-29 09:45:39 +0600

    Нахождение sqr числа

---
+++ Ex_Sem!1_Dem/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное
+//на само себя).● 4 -> 16 ● -3 -> 9 ● -7 -> 49 /
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+Console.WriteLine($"квадрат {number} = {sqr}");
