074314df4092da8a04f48e700e92a5b940ba7187
Igor <tararuev65@gmail.com>
2023-05-02 09:26:31 +0600

    Целые числа от N до-N

---
+++ Ex_Sem005/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает
+// все целые числа в промежутке от -N до N.
+
+Console.Clear();
+
+Console. WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int k = -N;
+
+while(k <= N)
+{
+    Console.Write($"{k} ");
+    k++;
+}
