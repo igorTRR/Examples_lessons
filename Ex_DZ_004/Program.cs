33c58f1c6a8aa9c79688871ffee0e11ed05aba64
Igor <tararuev65@gmail.com>
2023-05-02 23:17:00 +0600

    ДЗ-4 max из 3x

---
+++ Ex_DZ_004/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.;
+
+Console.Clear();
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число C: ");
+int C = int.Parse(Console.ReadLine()!);
+
+int max = A;
+
+if(B > A) max = B;
+     
+  if(B < C) max = C;
+    Console.Write($"max -> ");  
+    Console.Write(max);
+   
