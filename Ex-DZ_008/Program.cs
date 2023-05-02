63e4446a371d192b032e27a37f4edd4ca6da1246
Igor <tararuev65@gmail.com>
2023-05-02 23:55:47 +0600

    ДЗ-8 Чет-до n

---
+++ Ex-DZ_008/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число : ");
+int n = int.Parse(Console.ReadLine()!);
+
+int k = 2;
+while(k <= n)
+{
+    Console.Write($"{k}-> ");
+    k+= 2;
+}
