19b2f53ead9a077b546e4db21738c8f462d1b82d
Igor <tararuev65@gmail.com>
2023-05-02 19:20:06 +0600

    ДЗ-2 min-max заданных чисел

---
+++ Ex_DZ_02/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает
+// два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+int max = A ;
+int min  = B;
+
+if(A < B) max = B; 
+    Console.Write($"max -> ");  
+    Console.Write(max);
+    Console.WriteLine();
+    
+   if(A < B) min = A;
+    Console.Write($"min -> ");  
+    Console.Write(min);
+
+
+
+
+  
+  
+    
+
+
+    
