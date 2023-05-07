6770899ed23509d40baa22e37859916d3ae9c4f0
Igor <tararuev65@gmail.com>
2023-05-07 22:09:27 +0600

    Z011-Удалить 2цифру 3хзн.числа

--- Sem_Z011/Program.cs
+++ Sem_Z011/Program.cs
@@ -8,7 +8,9 @@ int num = new Random().Next(100, 1000);
 int a1 = num / 100;
 int a2 = num % 10;
 
-Console.Write($" {num} -> {a1}{a2}");
+int result = a1*10 +a2;
+
+Console.Write($" {num} -> {result}");
 
 
 
